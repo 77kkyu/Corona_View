923d845dc3c6749454ba07fcfd63fb6145acf574
77kkyu <62290111+77kkyu@users.noreply.github.com>
2020-07-23 10:35:30 +0900

    국내토탈현황 모듈화 진행중

---
+++ src/main/java/com/corona/main/KoreaTotalBean.java
@@ -0,0 +1,15 @@
+package com.corona.main;
+
+public class KoreaTotalBean {
+	
+	private String caseCount;
+	private String updateTime;
+	private String notcaseCount;
+	private String TotalChecking;
+	private String NowCase;
+	private String TotalCase;
+	private String TotalRecovered;
+	private String TotalDeath;
+	
+
+}
--- src/main/java/com/corona/main/MainJson.java
+++ src/main/java/com/corona/main/MainJson.java
@@ -46,26 +46,7 @@ public class MainJson {
 			jsonString1 += json1;
 		}
 		
-		
-		String address2 = "https://api.covid19api.com/summary";
-		BufferedReader br2;
-		URL url2;
-		HttpURLConnection conn2;
-		String protocol2 = "GET";
-		
-		url2 = new URL(address2);
-		conn2 = (HttpURLConnection)url2.openConnection();
-		conn2.setRequestMethod(protocol2);
-		br2 = new BufferedReader(new InputStreamReader(conn2.getInputStream()));
-		
-		String json2 = "";
-		while ((json2 = br2.readLine()) != null) {
-			jsonString2 += json2;
 			
-		}
-
-		
-		
 		// 네이버 뉴스 API
 		System.out.println("==== 검색 API 호출====");
 		String clientId = "v1vx3MIovnD2bp342z4k";
